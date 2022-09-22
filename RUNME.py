0209b0b1e83830fa5dcea041fd4de8ff4d5fde18
dbbnicole <dbbnicole>
2022-09-22 16:43:17 +0000

    Update DBR version to match PC

--- RUNME.py
+++ RUNME.py
@@ -35,7 +35,7 @@ from solacc.companion import NotebookSolutionCompanion
 cluster_json = {
     "num_workers": 8,
     "cluster_name": "MRA_cluster",
-    "spark_version": "10.4.x-scala2.12", # TODO: This needs to be updated to match JSL 4.1 
+    "spark_version": "9.1.x-cpu-ml-scala2.12",, # This needs to match the version of JSL in the Partner Connect
     "spark_conf": {
         "spark.serializer": "org.apache.spark.serializer.KryoSerializer",
         "spark.kryoserializer.buffer.max": "2000M",
